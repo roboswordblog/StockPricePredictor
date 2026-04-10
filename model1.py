27caeaea8c01eca966c34c8b4613f4fb59beaf22
Robosword <32asingh@stratfordschools.net>
2026-04-10 00:34:53 -0700

    Update model1.py
    Make the model, convert the dataframe into a tensor so that we can work with it.

--- model1.py
+++ model1.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 # Data Cleaning
 df = pd.read_csv("AAPL.csv")
@@ -6,3 +9,10 @@ df = df.drop(columns=["Open", "High", "Low"])
 df = df.iloc[2:]
 df.rename(columns={"Price": 'Date'}, inplace=True)
 
+# convert it into a tensor
+data = torch.tensor(df.values, dtype=torch.float32)
+
+# create the model class
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
